b379da738531f77ee1c20870315f6377f7ee4147
Devin MacGillivray <devhmac@gmail.com>
2024-03-23 16:35:46 -0600

    useRefs to target video element Passing ref to start video function

--- cameraFunctionality/camera/src/App.tsx
+++ cameraFunctionality/camera/src/App.tsx
@@ -1,22 +1,15 @@
 import { useState } from "react";
 import { cameraFunc } from "./utils/camera";
+// import { camera } from "./utils/altCam";
+import { Camera } from "./components/Camera.tsx";
 import "./App.css";
 
 function App() {
   const webcam = document.getElementById("webcam");
-
-  const takePhoto = (webcam) => {
-    webcam && webcam.snap();
-  };
   return (
     <>
       <h1>Camera Test</h1>
-      <img></img>
-      <div className="card">
-        <button onClick={cameraFunc}>enable Webcam</button>
-        <button onClick={() => takePhoto(webcam)}>capture</button>
-      </div>
-      <video id="webcam" width="640" height="480" autoPlay></video>
+      <Camera />
     </>
   );
 }
