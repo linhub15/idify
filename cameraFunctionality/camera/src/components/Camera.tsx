dba09f93f5c57e5b1e279710559689d5e9a0287d
Devin MacGillivray <devhmac@gmail.com>
2024-03-23 16:50:27 -0600

    screenshot taken

--- cameraFunctionality/camera/src/components/Camera.tsx
+++ cameraFunctionality/camera/src/components/Camera.tsx
@@ -1,15 +1,15 @@
 import { useRef } from "react";
-import { cameraFunc } from "../utils/camera";
+import { cameraFunc, takePhoto } from "../utils/camera";
 
 export const Camera = () => {
-  const videoRef = useRef<React.RefObject<HTMLDivElement | null>>(null);
+  const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
   return (
     <>
       <div className="card">
         <button onClick={() => cameraFunc(videoRef)}>enable Webcam</button>
-        {/* <button onClick={}>capture</button> */}
+        <button onClick={() => takePhoto(videoRef, canvasRef)}>capture</button>
       </div>
       <video
         // id="webcam"
@@ -18,6 +18,7 @@ export const Camera = () => {
         height="480"
         autoPlay
       ></video>
+      <canvas ref={canvasRef} width="640" height="480"></canvas>
     </>
   );
 };
