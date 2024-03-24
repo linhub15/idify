c1865c7331bcc47b684e3f6e9bc7a54da9843fe6
Hubert Lin <linhub15@gmail.com>
2024-03-23 22:14:19 -0600

    style: fix nav height

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,13 +3,13 @@ import { Nav } from "../components/Nav";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="flex flex-row h-screen">
-      <div className="flex w-72 flex-col h-full">
+    <div className="flex flex-row min-h-screen h-full">
+      <div className="flex w-72 flex-col">
         <Nav />
       </div>
       <div className="bg-slate-50 w-full h-full">
         <Outlet />
       </div>
-    </div>
+    </div>  
   ),
 });
