85b2203a4187c8a73fbb970580f9657aa5a13e03
Ian Baguio <ianrey_2004@yahoo.com>
2024-03-23 22:42:03 -0600

    chore: add features

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -1,3 +1,4 @@
+import { CameraIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
@@ -7,7 +8,53 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
   return (
     <div>
+      <Features />
       <Team />
+    </div>
+  );
+}
+
+function Features() {
+  const features = [
+    {
+      name: "Scan",
+      description: "Scan your document from your device's camera",
+      icon: CameraIcon,
+    },
+    {
+      name: "Auto-Fill Forms",
+      description: "Auto-Fill forms from your scanned document",
+      icon: PencilSquareIcon,
+    },
+  ];
+
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            ID-ify
+          </h2>
+          <dl className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2">
+            {features.map((feature) => (
+              <div key={feature.name}>
+                <dt className="text-base font-semibold leading-7 text-gray-900">
+                  <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                    <feature.icon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-1 text-base leading-7 text-gray-600">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
     </div>
   );
 }
@@ -57,10 +104,7 @@ function Team() {
           {people.map((person) => (
             <li key={person.name}>
               <div className="flex items-center gap-x-6">
-                <img
-                  className="h-16 w-16 rounded-full"
-                  src={person.imageUrl}
-                />
+                <img className="h-16 w-16 rounded-full" src={person.imageUrl} />
                 <div>
                   <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
                     {person.name}
