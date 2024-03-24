2605558998d7c57acb8a38defd53425e47070367
Ian Baguio <ianrey_2004@yahoo.com>
2024-03-23 18:38:58 -0600

    chore: add simple license form

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <>
+      <div className="md:flex md:items-center md:justify-between">
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl text-center font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+            ID-ify
+          </h2>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+}
