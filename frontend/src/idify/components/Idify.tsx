483dd624716854850d2ad2c1e0d47df7e7a97ff5
Devin MacGillivray <devhmac@gmail.com>
2024-03-23 23:14:51 -0600

    centering button

--- frontend/src/idify/components/Idify.tsx
+++ frontend/src/idify/components/Idify.tsx
@@ -11,7 +11,7 @@ export const Idify = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   return (
-    <div>
+    <div className="flex align-center justify-center">
       <dialog
         className={
           dialogOpen ? "flex flex-col justify-center items-center" : "hidden"
@@ -68,7 +68,7 @@ export const Idify = () => {
       </dialog>
 
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
         onClick={(e) => {
           e.preventDefault();
           enableCamera(videoRef);
